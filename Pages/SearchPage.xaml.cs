1a36a7a4c0f57da1e95921b8339046eb740eb21c
alex <aliceric27@gmail.com>
2025-11-11 22:54:16 +0800

    feat: 監聽語言變更事件並刷新 DataGrid 欄位標題

--- Pages/SearchPage.xaml.cs
+++ Pages/SearchPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using CN_GreenLumaGUI.Messages;
+using CN_GreenLumaGUI.tools;
+using CommunityToolkit.Mvvm.Messaging;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace CN_GreenLumaGUI.Pages
 {
@@ -10,6 +14,46 @@ namespace CN_GreenLumaGUI.Pages
 		public SearchPage()
 		{
 			InitializeComponent();
+
+			// 監聽語言變更事件，手動刷新 DataGrid 欄位標題
+			WeakReferenceMessenger.Default.Register<ConfigChangedMessage>(this, (r, m) =>
+			{
+				if (m.kind == nameof(DataSystem.Instance.LanguageCode))
+				{
+					RefreshDataGridColumns();
+				}
+			});
+		}
+
+		/// <summary>
+		/// 刷新 DataGrid 的欄位標題，強制重新評估 DynamicResource 綁定
+		/// </summary>
+		private void RefreshDataGridColumns()
+		{
+			// 取得所有欄位的 DynamicResource key
+			var columnHeaders = new[]
+			{
+				("Search_IndexColumn", 0),
+				("Search_CoverColumn", 1),
+				("Search_NameColumn", 2),
+				("Search_RatingColumn", 3),
+				("Search_StorePageColumn", 4),
+				("Search_AddToListColumn", 5)
+			};
+
+			// 手動更新每個欄位的 Header
+			foreach (var (resourceKey, columnIndex) in columnHeaders)
+			{
+				if (columnIndex < searchResList.Columns.Count)
+				{
+					var column = searchResList.Columns[columnIndex];
+					// 從資源字典中取得新的文字
+					if (Application.Current.TryFindResource(resourceKey) is string headerText)
+					{
+						column.Header = headerText;
+					}
+				}
+			}
 		}
 	}
 }
