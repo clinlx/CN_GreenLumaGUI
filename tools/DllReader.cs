2d694bc9b1306d1ded33026551e6be92dfe3432d
CLINLX <1302291929@qq.com>
2026-02-07 22:29:00 +0800

    Publish v1.6.9

--- tools/DllReader.cs
+++ tools/DllReader.cs
@@ -71,13 +71,13 @@ namespace CN_GreenLumaGUI.tools
         }
         public const int TotalMaxUnlockNum = 135; //GreenLuma最大支持到135的上限
         private const int intSize = 4;
-        private const int preNum = 16;
+        private const int preNum = 0;
         private static readonly byte[] prePattern =
         {
-            0xB8,0xCC,0xCC,0xCC,
-            0xCC,0xFF,0xD0,0x61,
-            0x9D,0xC3,0x01,0x00,
-            0x00,0x00,0x00,0x00,
+            0x5A,0x00,0x00,0x00,
+            0xCD,0x00,0x00,0x00,
+            0xDB,0x00,0x00,0x00,
+            0x36,0x01,0x00,0x00,
         };
         private static int[]? ReadAppListFromByte(byte[] data)
         {
@@ -96,6 +96,7 @@ namespace CN_GreenLumaGUI.tools
                         int val = BitConverter.ToInt32(arrayBytes, k * intSize);
                         if (val <= 1) break;
                         intArray.Add(val);
+                        // OutAPI.MsgBox($"{val}({k})");
                     }
                     return intArray.ToArray();
                 }
